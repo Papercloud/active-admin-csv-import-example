e679456c116cd998951d3fadaf16a9dac90cb4a3
Tomas Spacek <tomspacek@gmail.com>
2013-08-15 19:01:31 +1000

    Initial commit: new Rails 3.2.13 generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveAdminCsvImportExample::Application.config.secret_token = '2c67b42b24bfed92a4d431fb87bbecbbd8e3db7126024d42062ca5bfe6d5b3afdcc124ebff66acf89ca04396fb9cc80895a4d40df7126f11810c724c2243d75d'
